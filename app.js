abe92a4f946425d3a807c2bfb53d7ccac05f48fc
Bekzat <esimhanuli13@gmail.com>
2023-04-12 14:51:53 +0600

    begin of js

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
+let myLibrary = [];
+const title = document.querySelector('#info');
+const author = document.querySelector('#info1');
+const pages = document.querySelector('#info2');
+const read = document.querySelector('#info3');
+const btn = document.querySelector('#btn');
+const container = document.querySelector('.container');
+
 function Book(title, author, pages, read ){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.info = function () {
+     
         return this.title + ' by ' + this.author + ' ,' + this.pages + ' pages, ' + this.read;
     }
 }
 
+function addBookToLibrary(){
+    if(title.value.length === 0 || author.value.length === 0 || pages.value.length === 0){
+        alert("Please fill all the fields!");
+        return;
+    }
+    const newBook = new Book(title.value, author.value, pages.value, read.value);
+    myLibrary.push(newBook);
+}
+
+function clearValue() {
+    title.value = null;
+    author.value = null;
+    pages.value = null;
+    read.value = null;
+    return ;
+}
+
+
 let hobbit = new Book('The Hobbit','J.R.R. Tolkien', 295, 'not read yet');
 
-console.log(hobbit.info());
+btn.addEventListener('click', (e)=>{
+    e.preventDefault();
+    addBookToLibrary();
+    clearValue();
+})
