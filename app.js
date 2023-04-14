33024d87c8147986bd267628a259b4a43ae8e8de
Bekzat Yessimkhanuly <92264679+yessimkhanuly13@users.noreply.github.com>
2023-04-14 14:34:43 +0600

    Bookshelf modification

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const author = document.querySelector('#info1');
 const pages = document.querySelector('#info2');
 const read = document.querySelector('#info3');
 const btn = document.querySelector('#btn');
-const container = document.querySelector('.container');
+const bookshelf = document.querySelector('#bookshelf');
+const back  = document.querySelector('#return');
+const addBook = document.querySelector('.add');
+
 
 function Book(title, author, pages, read ){
     this.title = title;
@@ -34,11 +37,62 @@ function clearValue() {
     return ;
 }
 
+addBook.addEventListener('click', ()=>{
+    document.getElementById('popup').style.display="flex";
+    addBook.style.display = 'none';
+    bookshelf.textContent = '';
+    bookshelf.style.display = 'none';
+})
+
+back.addEventListener('click',()=>{
+    document.getElementById('popup').style.display="none";
+    addBook.style.display = 'block';
+})
+
+function createForm() {
+    myLibrary.forEach((element)=>{
+        let div = document.createElement('div');
+        div.classList.add('par');
+        let index = 0;
+        div.setAttribute('id',`index-${index}`);
+        for(let i = 0; i < 3; i++){
+            let span = document.createElement('p');
+            span.classList.add('span-form');
+            if(i === 0){
+                span.textContent = element.title;
+            }else if( i === 1){
+                span.textContent = element.author;
+            }else if (i === 2){
+                span.textContent = element.pages;
+            }
+            div.appendChild(span)
+        }
+
+        for(let i = 0; i < 2; i++){
+            let button = document.createElement('button');
+            if(i === 1){
+                button.textContent = "Delete";
+                button.setAttribute('id', `btn-${index}`);
+            }else{
+                button.textContent = element.read;
+                button.setAttribute('id', `btn2-${index}`);
+            }
+            div.appendChild(button);
+        }
+        
+        index++;
+        bookshelf.appendChild(div);
+    });
+}
 
 let hobbit = new Book('The Hobbit','J.R.R. Tolkien', 295, 'not read yet');
 
 btn.addEventListener('click', (e)=>{
+    addBook.style.display = 'block';
+    document.getElementById('popup').style.display= 'none';
+    bookshelf.style.display = 'grid';
     e.preventDefault();
     addBookToLibrary();
     clearValue();
+    createForm();
 })
